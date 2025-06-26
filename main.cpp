fcbd4cb6adb60301de80af4fee2b85e6bf1c6a8f
RajMagdum05 <rajmagdum6844@gmail.com>
2025-06-26 12:55:32 +0530

    Created Area function

--- main.cpp
+++ main.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 int add(int a, int b);
+int area(int length, int breadth);
 
 int main()
 {
-    int x=3,y=4;
+    int x=3, y=4, l=5, b=4;
     cout<<"Addition is: "<<add(x,y)<<endl;
+    cout<<"Area is: "<<area(l,b)<<endl;
     return 0;
 }
 int add(int a, int b)
 {
     return a+b;
+}
+int area(int length, int breadth)
+{
+    return length*breadth;
 }
