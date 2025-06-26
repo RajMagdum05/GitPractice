4c66b2756ed23b39d549d6a45c7ca4eb896e9d92
RajMagdum05 <rajmagdum6844@gmail.com>
2025-06-26 16:28:59 +0530

    Added class rectangle and functions in it.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,71 @@
 #include<iostream>
 using namespace std;
 
-int add(int a, int b);
-int area(int length, int breadth);
+class Rectangle
+{
+    private:
+            int length;
+            int breadth;
+    public: 
+            void setLength(int l);
+            void setBreadth(int b);
+
+            int getLength(){return length;}
+            int getBreadth(){return breadth;}
+
+            Rectangle(int length, int breadth)
+            {
+                setLength(length);
+                setBreadth(breadth);
+            }
+            int area();
+            int perimeter();
+};
+void Rectangle::setLength(int l)
+{
+    if(l<0)
+    {
+        cout<<"Entered length is invalid!!"<<endl;
+        exit(1);
+    }
+    else
+    {
+        length = l;
+    }
+}
+void Rectangle::setBreadth(int b)
+{
+    if(b<0)
+    {
+        cout<<"Entered breadth is invalid!!"<<endl;
+        exit(1);
+    }
+    else
+    {
+        breadth = b;
+    }
+}
+int Rectangle::area()
+{
+    return length*breadth;
+}
+int Rectangle::perimeter()
+{
+    return 2*(length+breadth);
+}
 
 int main()
 {
-    int x=3, y=4, l=5, b=4;
-    cout<<"Addition is: "<<add(x,y)<<endl;
-    cout<<"Area is: "<<area(l,b)<<endl;
+    int length, breadth;
+    cout<<"Enter length of the rectangle: ";
+    cin>>length;
+
+    cout<<"Enter breadth of the rectangle: ";
+    cin>>breadth;
+
+    Rectangle r(length,breadth);
+    cout<<"Area of the Reactangle is "<<r.area()<<"."<<endl;
+    cout<<"Perimeter of the Rectangle is "<<r.perimeter()<<"."<<endl;
+
     return 0;
-}
-int add(int a, int b)
-{
-    return a+b;
-}
-int area(int length, int breadth)
-{
-    return length*breadth;
 }
